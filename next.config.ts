f64f202f4eb892f91026a6afa2b7c3647fddb21e
Akzhol <akzhol.kasym04@gmail.com>
2025-10-14 19:23:46 +0500

    chore: config, utils

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+    additionalData: `@use 'settings/colors' as *; @use 'settings/variables' as *; @use 'settings/mixins' as *;`,
+  },
 };
 
 export default nextConfig;
